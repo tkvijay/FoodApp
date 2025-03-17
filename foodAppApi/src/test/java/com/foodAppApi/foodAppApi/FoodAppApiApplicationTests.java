2a0e69737ccbc82f8128be7fca652a78c7989f61
Vijay Tk <tkvijay12040@gmail.com>
2025-03-17 10:18:52 +0530

    FoodAPI commit

---
+++ foodAppApi/src/test/java/com/foodAppApi/foodAppApi/FoodAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodAppApi.foodAppApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
