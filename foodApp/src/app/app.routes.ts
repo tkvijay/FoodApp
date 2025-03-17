814ac782de1a349ff68d83b57c022d2f2e9c0a6b
Vijay Tk <tkvijay12040@gmail.com>
2025-03-17 15:07:25 +0530

    header stuff

--- foodApp/src/app/app.routes.ts
+++ foodApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './ui/content/menu/menu.component';
+import { HomeComponent } from './ui/content/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent ,pathMatch: 'full' },
+    { path: 'menu', component: MenuComponent ,pathMatch: 'full' },
+];
