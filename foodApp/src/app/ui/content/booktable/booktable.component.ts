1d83276808261fbba28846ca1c8929a1e20cf590
Vijay Tk <tkvijay12040@gmail.com>
2025-03-21 12:25:20 +0530

    Book table save done

--- foodApp/src/app/ui/content/booktable/booktable.component.ts
+++ foodApp/src/app/ui/content/booktable/booktable.component.ts
@@ -1,39 +1,46 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  Validators,
+  ReactiveFormsModule,
+} from "@angular/forms";
+
 
 @Component({
   selector: 'app-booktable',
   standalone: true,
   templateUrl: './booktable.component.html',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   styleUrls: ['./booktable.component.css']
 })
 export class BooktableComponent {
-  constructor(private http: HttpClient) {}
-  
-    public name: string = ''
-    public email: string= ''
-    public phone: string= ''
-    public  date: string= ''
-    public time: string= ''
-    public people: string= ''
-  
+
+  bookTableForm: FormGroup; // Main form group
+
+  constructor(private http: HttpClient,private fb: FormBuilder) {
+    this.bookTableForm = this.fb.group({
+      name: [""], // Simple input field
+      email: [""], // Another input field
+      phone: [""], // Another input field
+      date: [""], // Another input field
+      people: [""], // Another input field
+    });
+  }
+    
   onSubmit() 
   {
-    const name = this.name;
-    const email = this.email;
-    const phone = this.phone;
-    const date = this.date;
-    const time = this.time;
-    const numberOfPeople = this.people;
-   
-    const data = { name, email, phone, date, time, numberOfPeople };
+    
+    const data = this.bookTableForm.value;
+    console.log(data);
 
-    this.http.post('localhost:8080/api/booktable', data)
+    this.http.post('http://localhost:8080/api/booktable', data)
       .subscribe(response => {
         // Handle the response here
         console.log(response);
+        alert('Table Booked Successfully');
       });
   }
 
